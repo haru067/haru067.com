422a4073a7c7e192e1656c068fb783a772471854
haru067 <haru.j2@gmail.com>
2020-08-16 03:19:55 +0900

    Add rfa

---
+++ public/rfa/precache-manifest.902795f046932ef94478aa3b23f73151.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c2f976d1c2df93ed52cc7531430bd31c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "35edc527b3d5aad9baf9",
+    "url": "/static/css/main.de4c87ec.chunk.css"
+  },
+  {
+    "revision": "6f3f0002ba876381e347",
+    "url": "/static/js/2.7a6d2ac4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.7a6d2ac4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "35edc527b3d5aad9baf9",
+    "url": "/static/js/main.ad8bc84e.chunk.js"
+  },
+  {
+    "revision": "a85a43e54ac73fc64756",
+    "url": "/static/js/runtime-main.d1920d30.js"
+  }
+]);
